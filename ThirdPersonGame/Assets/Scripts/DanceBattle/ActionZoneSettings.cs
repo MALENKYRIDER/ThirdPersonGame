a81f00150e8b277bf2e008effd430c85c58bd35d
MALENKYRIDER <klishevichmikita@gmail.com>
2026-04-05 12:12:46 +0200

    added particles, second button, paticles for SpecialDance

---
+++ ThirdPersonGame/Assets/Scripts/DanceBattle/ActionZoneSettings.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace DanceBattle
+{
+    [CreateAssetMenu(menuName = nameof(DanceBattle) + "/" + nameof(ActionZoneSettings),
+        fileName = nameof(ActionZoneSettings))]
+    public class ActionZoneSettings : ScriptableObject
+    {
+        [SerializeField] private float _playDuration = 2f;
+        [SerializeField] private float _playThreshhold = 1.2f;
+        [SerializeField] private Vector2 _playScale = new(1.75f, 0.8f);
+        [SerializeField] private Color _failColor = new Color(1f, 0f, 0f, 0f);
+        [SerializeField] private float _failDuration = 0.3f;
+        
+        public float PlayDuration => _playDuration;
+        public float PlayThreshhold => _playThreshhold;
+        public Vector2 PlayScale => _playScale;
+        public Color FailColor => _failColor;
+        public float FailDuration => _failDuration;
+    }
+}
---
+++ ThirdPersonGame/Assets/Scripts/DanceBattle/ScoreView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace DanceBattle
+{
+    public class ScoreView : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _score;
+        [SerializeField] private GameCore _gameCore;
+
+        private void OnEnable()
+        {
+            _gameCore.ScoreChanged += OnScoreChanged;
+        }
+
+        private void OnDisable()
+        {
+            _gameCore.ScoreChanged -= OnScoreChanged;
+        }
+
+        private void OnScoreChanged(int value)
+        {
+            _score.text = value.ToString();
+        }
+    }
+}
